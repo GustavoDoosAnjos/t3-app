d8efad3a2eff981cc966525fe6cb8a31fef4ad61
GustavodoosAnjos <anjos.gu@gmail.com>
2023-06-08 22:00:26 -0300

    rota de POST

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,57 +5,91 @@ import Image from "next/image";
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime"
+import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+  const ctx = api.useContext();
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate()
+    }
+  });
+  const [input, setInput] = useState("");
 
-  if(!user) return null;
+  if (!user) return null;
 
   return (
-  <div className="flex w-full gap-3">
-    <Image className="flex h-14 w-14 rounded-full" src={user.profileImageUrl} alt="profile image" width={56} height={56}/>
-    <input placeholder="passe o papo" className="grow bg-transparent outline-none"/>
-  </div>
-  )
-}
+    <div className="flex w-full gap-3">
+      <Image
+        className="flex h-14 w-14 rounded-full"
+        src={user.profileImageUrl}
+        alt="profile image"
+        width={56}
+        height={56}
+      />
+      <input
+        placeholder="passe o papo"
+        className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
+      />
+      <button onClick={() => mutate({ content: input })}>Post</button>
+    </div>
+  );
+};
 
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 const PostView = (props: PostWithUser) => {
-  const {post, author} = props;
+  const { post, author } = props;
   return (
-    <div key={post.id} className="flex p-4 border-b border-slate-300 gap-3">
-      <Image className="h-14 w-14 rounded-full" src={author.profileImageUrl} alt={`@${author.username}'s profile picture`} width={56} height={56}/>
+    <div key={post.id} className="flex gap-3 border-b border-slate-300 p-4">
+      <Image
+        className="h-14 w-14 rounded-full"
+        src={author.profileImageUrl}
+        alt={`@${author.username}'s profile picture`}
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col">
-        <div className="flex text-slate-400 gap-2"><span>{`@${author.username}`}</span><span>{`·  ${dayjs(post.createdAt).fromNow()}`}</span></div>
+        <div className="flex gap-2 text-slate-400">
+          <span>{`@${author.username}`}</span>
+          <span>{`·  ${dayjs(post.createdAt).fromNow()}`}</span>
+        </div>
         <span>{post.content}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
-  if (postsLoading) return <LoadingPage/>
+  if (postsLoading) return <LoadingPage />;
 
-  if (!data) return <div>Something went wrong</div>
+  if (!data) return <div>Something went wrong</div>;
 
   return (
     <div className="flex flex-col">
-      {data?.map((fullPost) => (<PostView {...fullPost} key={fullPost.post.id}/>))}
+      {data?.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const Home: NextPage = () => {
   const { isLoaded: userLoaded, isSignedIn } = useUser();
   api.posts.getAll.useQuery();
 
-  if (!userLoaded) return <div/>
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -65,13 +99,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen justify-center">
-        <div className="w-full md:max-w-2xl border-x border-slate-400">
+        <div className="w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4 ">
-            {!isSignedIn && <div className="flex justify-center"><SignInButton/></div>}
-            {!!userLoaded && <CreatePostWizard/>}
+            {!isSignedIn && (
+              <div className="flex justify-center">
+                <SignInButton />
+              </div>
+            )}
+            {!!userLoaded && <CreatePostWizard />}
           </div>
 
-          <Feed/>
+          <Feed />
         </div>
       </main>
     </>
